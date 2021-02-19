eeed9d32d9feee2b1cbf196b54ca3b17c5defe48
Onyekachukwu Gibson Ekezie <57141168+goekezie@users.noreply.github.com>
2021-02-19 10:21:44 +0100

    Update settings.py

--- devend/settings.py
+++ devend/settings.py
@@ -79,20 +79,27 @@ WSGI_APPLICATION = 'devend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'devend',
-        'USER': 'dev',
-        'PASSWORD': 'Adikaibe@99',
-        'HOST': 'localhost',
-        'PORT': '',
-        'TEST': {
-            'NAME': 'test_devend',
-        },
+if sys.argv[1] == 'test':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'devend',
+            'USER': 'dev',
+            'PASSWORD': 'Adikaibe@99',
+            'HOST': 'localhost',
+            'PORT': '',
+            'TEST': {
+                'NAME': 'test_devend',
+            },
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
