5576b8591fe79c46ef83ad5aecce15b22b47e75c
Onyekachukwu Gibson Ekezie <57141168+goekezie@users.noreply.github.com>
2021-02-23 11:19:57 +0100

    Update settings.py

--- devend/settings.py
+++ devend/settings.py
@@ -86,18 +86,11 @@ if sys.argv[1] == 'test':
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-else:
+else: 
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'devend',
-            'USER': 'dev',
-            'PASSWORD': 'Adikaibe@99',
-            'HOST': 'localhost',
-            'PORT': '',
-            'TEST': {
-                'NAME': 'test_devend',
-            },
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
